4afc70989bf73c4d89239927c60670ad06f232e2
Shakhzod Ikromov <aabbcc.double@gmail.com>
2020-02-12 16:18:56 +0300

    Remove unnecessary files

--- Example/OCMockitoSwiftExampleApp/ViewController.m
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright © 2018 Aleksander Zubala. All rights reserved.
-//
-
-#import "ViewController.h"
-
-@implementation ViewController
-
-@end
--- Example/OCMockitoSwiftTests/OCMockitoSwiftTests-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "TestClass.h"
-#import "TestProtocol.h"
-#import "ObjectiveCException.h"
-
-#import <OCMockitoSwift/OCMockitoSwift.h>
